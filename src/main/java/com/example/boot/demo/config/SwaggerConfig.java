1be307a56538eefd63e2454e293b22b5c1d08d1c
kiriuzero <kiriuzero@outlook.com>
2019-01-07 12:06:37 +0800

    no message

---
+++ src/main/java/com/example/boot/demo/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.example.boot.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * <Description> <br>
+ *
+ * @author luoluocaihong<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate Oct 24, 2016 <br>
+ * @since V8.1<br>
+ */
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
+//@ComponentScan(basePackages = { "XXXX" })
+public class SwaggerConfig {
+    //swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.example.Controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("Swagger2 RESTful API")
+                //创建人
+                .contact(new Contact("Anakin", "http://www.baidu.com", ""))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
+    }
+
+}
